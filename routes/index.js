d22f8e117dbf41e5fb9426acf05310f39bfbc2cd
Apurva khadkikar <apurvakhadkikar1901@gmail.com>
2020-08-28 08:29:41 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.get('/', (req, res, next) => {
     } else {
 
       viewData = {
-        title: 'Home',
+        title: 'UploadImages',
         viewName: 'imageuploder',
         accountName: config.getStorageAccountName(),
         containerName: containerName
